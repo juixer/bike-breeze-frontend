434dd412478534b1e2cfdf3f9cc6c70e7005a522
Ashfakul Islam <r4pido3@gmail.com>
2024-08-26 11:39:54 +0600

    auth slice created

--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -1,5 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../store";
+
+export type TUser = {
+  email: string;
+  role: string;
+  iat: number;
+  exp: number;
+};
+
 type TInitialState = {
-    email: string,
-    role: string,
-    profile_picture: string,
-}
+  user: null | TUser;
+  token: string | null;
+};
+
+const initialState: TInitialState = {
+  user: null,
+  token: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const { user, token } = action.payload;
+      state.user = user;
+      state.token = token;
+    },
+    logOut: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { logOut, setUser } = authSlice.actions;
+
+export default authSlice.reducer;
+
+export const useCurrentToken = (state: RootState) => state.auth.token;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { baseApi } from "./api/baseApi";
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
-import numberReducer from "./features/number/numberSlice"
+import authReducer from "./features/auth/authSlice"
 import storage from 'redux-persist/lib/storage'
 
 const persistConfig = {
@@ -9,12 +9,12 @@ const persistConfig = {
     storage
 }
 
-const persistedNumberReducer = persistReducer(persistConfig, numberReducer)
+const persistedNumberReducer = persistReducer(persistConfig, authReducer)
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
-    number: persistedNumberReducer,
+    auth: persistedNumberReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
