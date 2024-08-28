8714d6b0dd80c659d6cf10093cdcb27626832895
Ashfakul Islam <r4pido3@gmail.com>
2024-08-29 01:25:14 +0600

    some issue fixed

--- src/Components/Coupon/Coupon.tsx
+++ src/Components/Coupon/Coupon.tsx
@@ -29,19 +29,33 @@ const Coupon = () => {
               src="https://i.ibb.co/0Dn7L6k/nocoupon-removebg-preview.png"
               className="w-48"
             />
-            <h1 className="text-3xl font-bold uppercase">
-              Sorry
-            </h1>
+            <h1 className="text-3xl font-bold uppercase">Sorry</h1>
             <h1 className="text-xl font-bold uppercase">
               there are no coupons available right now
             </h1>
           </div>
         ) : (
-          <div className="flex justify-center items-center gap-5">
-            {availableCoupon.data.map((coupon: TCoupon, index: string) => (
-              <CouponCard key={index} coupon={coupon} />
-            ))}
-          </div>
+          <>
+            <div className="flex justify-center items-center gap-5">
+              {availableCoupon.data.map((coupon: TCoupon, index: string) => (
+                <CouponCard key={index} coupon={coupon} />
+              ))}
+            </div>
+
+            <div className="collapse collapse-plus bg-base-200 rounded-md max-w-4xl mx-auto">
+              <input type="radio" name="my-accordion-3" defaultChecked />
+              <div className="collapse-title text-xl font-medium">
+                How to apply this
+              </div>
+              <div className="collapse-content">
+                <p>
+                  To apply this coupon, visit the 'My Rental' page in your
+                  dashboard, click on 'Pay,' and you'll be able to enter your
+                  coupon there.
+                </p>
+              </div>
+            </div>
+          </>
         )}
       </div>
     </div>
